e75e2a834e287d0b552ad3514a70ecbc8502ca80
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-23 23:44:12 +0800

    Delete graphs directory

--- graphs/plotWAV.sh
+++
@@ -1,7 +0,0 @@
-rm tmp/plot*
-
-ffmpeg -i $1 -ac 2 -filter_complex:a '[0:a]aresample=8000,asplit[l][r]' \
-	-map '[l]' -c:a pcm_s16le -f data tmp/plot-waveform-ac1 \
-	-map '[r]' -c:a pcm_s16le -f data tmp/plot-waveform-ac2
-
-gnuplot plot.p
--- graphs/xticgen.py
+++
@@ -1,2 +0,0 @@
-for i in range(1,(10*60)+1):
-	print("\"" + str(i) + "\" " + str((i*16000)) + ", ",end='')
